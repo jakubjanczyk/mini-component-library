5a5a7cebd9ca9d13748e312c5b5429ab5c4b238b
Jakub Janczyk <kubex321@gmail.com>
2021-11-26 18:36:15 +0100

    Select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -5,13 +5,62 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
 
+const BuiltInSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  appearance: none;
+  cursor: pointer;
+`
+
+const Wrapper = styled.div`
+  width: max-content;
+  
+  color: ${COLORS.gray700};
+  font-size: ${16 / 16}rem;
+  
+  position: relative;
+  
+  &:hover {
+    color: ${COLORS.black};
+  }
+`
+
+const Content = styled.div`
+  height: 100%;
+  border-radius: 8px;
+  padding: 12px 16px;
+  display: flex;
+  align-items: center;
+  
+  background-color: ${COLORS.transparentGray15};
+  
+  ${BuiltInSelect}:focus + & {
+    outline: 1px solid -webkit-focus-ring-color;
+  }
+`
+
+const IconUI = styled(Icon)`
+  margin-left: 24px;
+`
+
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <BuiltInSelect value={value} onChange={onChange}>
+        {children}
+      </BuiltInSelect>
+      <Content>
+        {displayedValue}
+        <IconUI id="chevron-down" />
+      </Content>
+    </Wrapper>
+
   );
 };
 
