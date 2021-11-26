a1a8e852d76df86ce79e7668c5c2b5857e59b480
Jakub Janczyk <kubex321@gmail.com>
2021-11-26 18:08:18 +0100

    progressbar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,43 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from 'react'
+import styled, { css } from 'styled-components'
+import { COLORS } from '../../constants'
+import VisuallyHidden from '../VisuallyHidden'
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+const SIZE_HEIGHT = {
+  large: 24,
+  medium: 12,
+  small: 8
+}
 
-const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
-};
+const Container = styled.div`
+  width: 370px;
+  height: ${({ size }) => `${SIZE_HEIGHT[size]}px`};
+  background-color: ${COLORS.transparentGray15};
+  box-shadow: inset 0 2px 4px rgba(128, 128, 128, 0.35);
+  border-radius: 8px;
+  padding: ${({ size }) => size === 'large' ? '4px' : '0'};
+`
 
-export default ProgressBar;
+const Bar = styled.div`
+  width: ${({ value }) => `${value}%`};
+  background-color: ${COLORS.primary};
+  height: 100%;
+  border-radius: 4px;
+
+  ${({ value }) => value !== 100 && css`
+    border-top-right-radius: 0;
+    border-bottom-right-radius: 0;
+  `}
+`
+
+const ProgressBar = ({ value = 0, size = 'medium' }) => {
+  return (
+    <Container size={size} role="progressbar" aria-valuenow={value} aria-valuemin="0" aria-valuemax="100">
+      <Bar value={value} />
+      <VisuallyHidden>{value}%</VisuallyHidden>
+    </Container>
+  )
+}
+
+export default ProgressBar
